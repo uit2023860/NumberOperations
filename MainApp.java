36266917e1f9e452bd4139deb76b259ff5236447
uit2023860 <ankita.sarvale@cumminscollege.in>
2026-01-30 12:40:02 +0530

    committing changes from git

--- MainApp.java
+++ MainApp.java
@@ -1,1 +1,20 @@
 
+import java.util.Scanner;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        System.out.println("Is Prime: " + PrimeOperations.isPrime(n));
+        System.out.print("Fibonacci: ");
+        FibonacciOperations.fibonacci(n);
+        System.out.println("Factorial: " + FactorialOperations.factorial(n));
+
+        sc.close();
+    }
+}
+
